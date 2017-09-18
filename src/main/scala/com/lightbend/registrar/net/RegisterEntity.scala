3f128f88a385f4f3099b63c45cc1f9a7bbb5949b
Jason Longshore <longshorej@gmail.com>
2017-09-18 18:25:15 -0500

    Remove unused class

--- src/main/scala/com/lightbend/registrar/net/RegisterEntity.scala
+++
@@ -1,9 +0,0 @@
-package com.lightbend.registrar.net
-
-import spray.json.DefaultJsonProtocol._
-
-object RegisterEntity {
-  implicit val registerEntityFormat = jsonFormat2(RegisterEntity.apply)
-}
-
-case class RegisterEntity(topic: String, name: String)
